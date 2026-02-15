2f831d01d0a18137925927a835f8fe010b9561bc
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-02-16 00:39:22 +0300

    Update queue.py

--- codes/non-primitive/linear/queue.py
+++ codes/non-primitive/linear/queue.py
@@ -1,1 +1,15 @@
+# Queue implementation using deque
+
+from collections import deque
+
+queue = deque()
+
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue:", queue)
+
+queue.popleft()
+print("After dequeue:", queue)
 

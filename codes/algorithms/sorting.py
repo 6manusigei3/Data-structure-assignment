8594464713f2c59010ea2c3262f044f1ae777f2d
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-02-16 00:49:37 +0300

    Update sorting.py

--- codes/algorithms/sorting.py
+++ codes/algorithms/sorting.py
@@ -1,1 +1,15 @@
+# Bubble Sort Algorithm
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+data = [64, 34, 25, 12, 22, 11, 90]
+
+print("Original array:", data)
+bubble_sort(data)
+print("Sorted array:", data)
 
